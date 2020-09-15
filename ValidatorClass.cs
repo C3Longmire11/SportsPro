31e217a37aa94bdd438cbc5524ae1d73cc595ce8
Conor Longmire <55465929+C3Longmire11@users.noreply.github.com>
2020-09-15 16:22:52 -0500

    Add files via upload

---
+++ ValidatorClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportsProUserInterface
+{
+    public static class ValidatorClass
+    {
+        public static bool IsPresent(string txtInput)
+        {
+            if (txtInput == string.Empty)
+            { return false; }
+            else
+            { return true; }
+        }
+        public static bool IsNumeric(string txtInput)
+        {
+            Int32 number;
+            try
+            {
+                number = Convert.ToInt32(txtInput);
+                return true;
+            }
+            catch
+            { return false; }
+        }
+    }
+}
