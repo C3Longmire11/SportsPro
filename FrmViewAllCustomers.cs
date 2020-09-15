8e9431523f8fd3cff540259b40b63c55fce74fab
Conor Longmire <55465929+C3Longmire11@users.noreply.github.com>
2020-09-15 16:20:12 -0500

    Add files via upload

---
+++ FrmViewAllCustomers.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SportsProBusinessClassLibrary;
+
+namespace SportsProUserInterface
+{
+    public partial class FrmViewAllCustomers : Form
+    {
+        CustomerBL customerBusinessLogic = new CustomerBL();
+        public FrmViewAllCustomers()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnHideAllCustomers_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+
+        private void FrmViewAllCustomers_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                dgvAllCustomers.DataSource = customerBusinessLogic.GetAllCustomers();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, ex.GetType().ToString(), MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+    }
+}
